8eb5bfee726af3f8a3259ca3a5af53654860504d
Eduardo Gonik <eduardogonik@gmail.com>
2026-02-22 18:56:17 -0300

    [vibe] up2

--- run-from-script.sh
+++ run-from-script.sh
@@ -5,5 +5,5 @@ docker compose up -d
 sleep 3
 diesel setup
 cargo build --release --bin convert-invert
-SERVER_BIND=127.0.0.1:8081 WORKER_COUNT=4 WORKER_BIN=target/release/convert-invert cargo run --release --bin trigger_server
+SERVER_BIND=127.0.0.1:8081 WORKER_COUNT=4 WORKER_BIN=target/release/convert-invert cargo run --release --bin trigger_server | tee logs-script-multiplex
 # cargo run --release | tee logs-script
